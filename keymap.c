485b465cb5b02db90a003d064950ba2f3eefe7ef
novellito <novellito7@gmail.com>
2020-08-17 21:51:46 -0700

    adjust variables

--- keymap.c
+++ keymap.c
@@ -2,10 +2,10 @@
 
 extern keymap_config_t keymap_config;
 
-#define _MAIN 0
-#define _FN1 1
-#define _FN2 2
-#define _FN3 3
+#define _QWERTY 0
+#define _LOWER 1
+#define _RAISE 2
+#define _ADJUST 3
 
 #define CODE_UP RALT(KC_UP)
 #define CODE_DOWN RALT(KC_DOWN)
@@ -25,15 +25,14 @@ extern keymap_config_t keymap_config;
 #define RT_3RD LCTL(RALT(KC_G))
 
 enum custom_keycodes {
-  _MAIN = SAFE_RANGE,
-  _FN1,
-  _FN2,
-  _FN3,
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  ADJUST,
 };
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_MAIN] = LAYOUT(
+  [_QWERTY] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                       KC_6,    KC_7,    KC_8,    KC_9,      KC_0,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -47,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_FN1] = LAYOUT(
+  [_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      GRAVE_ESC, _______, _______,   _______, _______,  _______,                            _______, _______, _______, KC_MINS, KC_EQL, KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -61,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_FN2] = LAYOUT(
+  [_RAISE] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -75,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_FN3] = LAYOUT(
+  [_ADJUST] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      LOCK_MAC, _______, _______, _______, _______, _______,                            RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RESET,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -111,35 +110,35 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
-        set_single_persistent_default_layer(_MAIN);
+        set_single_persistent_default_layer(_QWERTY);
       }
       return false;
       break;
     case LOWER:
       if (record->event.pressed) {
-        layer_on(_FN1);
-        update_tri_layer(_FN1, _FN2, _FN3);
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
-        layer_off(_FN1);
-        update_tri_layer(_FN1, _FN2, _FN3);
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
       break;
     case RAISE:
       if (record->event.pressed) {
-        layer_on(_FN2);
-        update_tri_layer(_FN1, _FN2, _FN3);
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
-        layer_off(_FN2);
-        update_tri_layer(_FN1, _FN2, _FN3);
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
       break;
     case ADJUST:
       if (record->event.pressed) {
-        layer_on(_FN3);
+        layer_on(_ADJUST);
       } else {
-        layer_off(_FN3);
+        layer_off(_ADJUST);
       }
       return false;
       break;
@@ -149,13 +148,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
-    case _FN1:
+    case _LOWER:
         rgblight_setrgb (RGB_TURQUOISE);
         break;
-    case _FN2:
+    case _RAISE:
         rgblight_setrgb (RGB_PURPLE);
         break;
-    case _FN3:
+    case _ADJUST:
         rgblight_setrgb (RGB_YELLOW);
         break;
     default:
